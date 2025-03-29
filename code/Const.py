cc77012cb37c63bd2ff6e27bfad823694c8bf0ba
Wadson <wadson@gmail.com>
2025-03-29 15:12:40 -0300

    Creating Player and Enemies Moving players and Enemies

--- code/Const.py
+++ code/Const.py
@@ -1,4 +1,6 @@
 # C
+import pygame
+
 COLOR_SILVER = (171, 167, 76)
 COLOR_WHITE = (255, 255, 255)
 COLOR_BLACK = (0, 0, 0)
@@ -9,6 +11,7 @@ COLOR_PINK = (234, 54, 128)
 COLOR_PURPLE = (116, 27, 124)
 
 # E
+EVENT_ENEMY = pygame.USEREVENT + 1
 ENTITY_SPEED = {
     'Level1Bg0' : 0,
     'Level1Bg1' : 1,
@@ -16,13 +19,34 @@ ENTITY_SPEED = {
     'Level1Bg3' : 3,
     'Level1Bg4' : 4,
     'Level1Bg5' : 5,
+    'Player1'   : 6,
+    'Player2'   : 6,
+    'Enemy1'    : 2,
+    'Enemy2'    : 1,
+    'Enemy3'    : 2,
 }
 
 # M
 MENU_OPTION = ('NEW GAME',
                'COOP',
+               'VERSUS',
                'SCORE',
                'EXIT')
+
+# P
+PLAYER_KEY_UP = {'Player1': pygame.K_w,
+                 'Player2':pygame.K_UP}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_s,
+                 'Player2':pygame.K_DOWN}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_a,
+                 'Player2':pygame.K_LEFT}
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_d,
+                 'Player2':pygame.K_RIGHT}
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_LSHIFT,
+                 'Player2':pygame.K_RSHIFT}
+
+# S
+SPAWN_TIME = 4000
 
 #  W
 WIN_WIDTH = 1920

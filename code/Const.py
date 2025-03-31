38b72ec282aa8d811510edad9a01cbb137593ccc
Wadson <wadson@gmail.com>
2025-03-30 23:41:56 -0300

    Creating Player and Enemies Shoot

--- code/Const.py
+++ code/Const.py
@@ -13,19 +13,6 @@ COLOR_ORANGE = (240, 134, 80)
 
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
-ENTITY_SPEED = {
-    'Level1Bg0' : 0,
-    'Level1Bg1' : 1,
-    'Level1Bg2' : 2,
-    'Level1Bg3' : 3,
-    'Level1Bg4' : 4,
-    'Level1Bg5' : 5,
-    'Player1'   : 6,
-    'Player2'   : 6,
-    'Enemy1'    : 3,
-    'Enemy2'    : 2,
-    'Enemy3'    : 3,
-}
 
 ENTITY_HEALTH = {
     'Level1Bg0': 999,
@@ -35,10 +22,42 @@ ENTITY_HEALTH = {
     'Level1Bg4': 999,
     'Level1Bg5': 999,
     'Player1'  : 300,
+    'Player1Shoot': 1,
     'Player2'  : 300,
+    'Player2Shoot': 1,
     'Enemy1'   : 50,
+    'Enemy1Shoot': 1,
     'Enemy2'   : 60,
+    'Enemy2Shoot': 1,
     'Enemy3'   : 70,
+    'Enemy3Shoot': 1,
+}
+
+ENTITY_SHOOT_DELAY = {
+    'Player1' : 20,
+    'Player2' : 15,
+    'Enemy1'  : 100,
+    'Enemy2'  : 200,
+    'Enemy3'  : 250,
+}
+
+ENTITY_SPEED = {
+    'Level1Bg0'    : 0,
+    'Level1Bg1'    : 1,
+    'Level1Bg2'    : 2,
+    'Level1Bg3'    : 3,
+    'Level1Bg4'    : 4,
+    'Level1Bg5'    : 5,
+    'Player1'      : 3,
+    'Player1Shoot' : 1,
+    'Player2'      : 3,
+    'Player2Shoot' : 3,
+    'Enemy1'       : 1,
+    'Enemy1Shoot'  : 5,
+    'Enemy2'       : 1,
+    'Enemy2Shoot'  : 2,
+    'Enemy3'       : 1,
+    'Enemy3Shoot'  : 2,
 }
 
 # M
@@ -49,16 +68,16 @@ MENU_OPTION = ('NEW GAME',
                'EXIT')
 
 # P
-PLAYER_KEY_UP = {'Player1': pygame.K_w,
-                 'Player2':pygame.K_UP}
-PLAYER_KEY_DOWN = {'Player1': pygame.K_s,
-                 'Player2':pygame.K_DOWN}
-PLAYER_KEY_LEFT = {'Player1': pygame.K_a,
-                 'Player2':pygame.K_LEFT}
+PLAYER_KEY_UP =    {'Player1': pygame.K_w,
+                   'Player2':pygame.K_UP}
+PLAYER_KEY_DOWN =  {'Player1': pygame.K_s,
+                   'Player2':pygame.K_DOWN}
+PLAYER_KEY_LEFT =  {'Player1': pygame.K_a,
+                   'Player2':pygame.K_LEFT}
 PLAYER_KEY_RIGHT = {'Player1': pygame.K_d,
-                 'Player2':pygame.K_RIGHT}
+                   'Player2':pygame.K_RIGHT}
 PLAYER_KEY_SHOOT = {'Player1': pygame.K_LSHIFT,
-                 'Player2':pygame.K_RSHIFT}
+                   'Player2':pygame.K_RSHIFT}
 
 # S
 SPAWN_TIME = 4000

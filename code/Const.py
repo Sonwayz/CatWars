8cafe4117273e45bc3704bc3581a2a1b39de0c7e
Wadson <wadson@gmail.com>
2025-04-02 12:16:09 -0300

    Level 2 implementation

--- code/Const.py
+++ code/Const.py
@@ -14,7 +14,7 @@ C_CYAN = (0, 128, 128)
 
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
-
+EVENT_TIMEOUT = pygame.USEREVENT + 2
 ENTITY_DAMAGE =  {
     'Level1Bg0': 0,
     'Level1Bg1': 0,
@@ -23,7 +23,15 @@ ENTITY_DAMAGE =  {
     'Level1Bg4': 0,
     'Level1Bg5': 0,
     'Level1Bg6': 0,
-    'Level1Bg7': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
+    'Level2Bg5': 0,
+    'Level2Bg6': 0,
+    'Level2Bg7': 0,
+    'Level2Bg8': 0,
     'Player1'  : 1,
     'Player1Shoot': 25,
     'Player2'  : 1,
@@ -44,7 +52,15 @@ ENTITY_HEALTH = {
     'Level1Bg4': 999,
     'Level1Bg5': 999,
     'Level1Bg6': 999,
-    'Level1Bg7': 999,
+    'Level2Bg0': 999,
+    'Level2Bg1': 999,
+    'Level2Bg2': 999,
+    'Level2Bg3': 999,
+    'Level2Bg4': 999,
+    'Level2Bg5': 999,
+    'Level2Bg6': 999,
+    'Level2Bg7': 999,
+    'Level2Bg8': 999,
     'Player1'  : 300,
     'Player1Shoot': 1,
     'Player2'  : 300,
@@ -73,6 +89,15 @@ ENTITY_SPEED = {
     'Level1Bg4'    : 4,
     'Level1Bg5'    : 5,
     'Level1Bg6'    : 6,
+    'Level2Bg0'    : 0,
+    'Level2Bg1'    : 1,
+    'Level2Bg2'    : 2,
+    'Level2Bg3'    : 3,
+    'Level2Bg4'    : 4,
+    'Level2Bg5'    : 5,
+    'Level2Bg6'    : 6,
+    'Level2Bg7'    : 7,
+    'Level2Bg8'    : 8,
     'Player1'      : 3,
     'Player1Shoot' : 5,
     'Player2'      : 3,
@@ -93,7 +118,15 @@ ENTITY_SCORE = {
     'Level1Bg4'    : 0,
     'Level1Bg5'    : 0,
     'Level1Bg6'    : 0,
-    'Level1Bg7'    : 0,
+    'Level2Bg0'    : 0,
+    'Level2Bg1'    : 0,
+    'Level2Bg2'    : 0,
+    'Level2Bg3'    : 0,
+    'Level2Bg4'    : 0,
+    'Level2Bg5'    : 0,
+    'Level2Bg6'    : 0,
+    'Level2Bg7'    : 0,
+    'Level2Bg8'    : 0,
     'Player1'      : 0,
     'Player1Shoot' : 0,
     'Player2'      : 0,
@@ -126,7 +159,11 @@ PLAYER_KEY_SHOOT = {'Player1': pygame.K_LSHIFT,
                    'Player2':pygame.K_RSHIFT}
 
 # S
-SPAWN_TIME = 4000
+SPAWN_TIME = 1000
+
+# T
+TIMEOUT_STEP = 100 # 100ms
+TIMEOUT_LEVEL = 20000 #60 segundos
 
 #  W
 WIN_WIDTH = 1920
--- code/Game.py
+++ code/Game.py
@@ -17,10 +17,14 @@ class Game:
             menu = Menu(self.window)
             menu_return = menu.run()
 
-
             if menu_return in [MENU_OPTION[0], MENU_OPTION[1]]:
-                level = Level(self.window, 'Level1', menu_return)
-                level_return = level.run()
+                player_score = [0, 0] # [Player1, Player2]
+                level = Level(self.window, 'Level1', menu_return, player_score)
+                level_return = level.run(player_score)
+                if level_return:
+                    level = Level(self.window, 'Level2', menu_return, player_score)
+                    level_return = level.run(player_score)
+
             elif menu_return == MENU_OPTION[3]:
                 pygame.quit()  # Close Window
                 quit()  # end pygame

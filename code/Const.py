7a8d94d89e57be0b7fb05e2e03f55bc77136007e
Wadson <wadson@gmail.com>
2025-04-02 13:59:54 -0300

    Aula 08 metade

--- code/Const.py
+++ code/Const.py
@@ -15,7 +15,7 @@ C_CYAN = (0, 128, 128)
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
 EVENT_TIMEOUT = pygame.USEREVENT + 2
-ENTITY_DAMAGE =  {
+ENTITY_DAMAGE = {
     'Level1Bg0': 0,
     'Level1Bg1': 0,
     'Level1Bg2': 0,
@@ -32,15 +32,15 @@ ENTITY_DAMAGE =  {
     'Level2Bg6': 0,
     'Level2Bg7': 0,
     'Level2Bg8': 0,
-    'Player1'  : 1,
+    'Player1': 1,
     'Player1Shoot': 25,
-    'Player2'  : 1,
+    'Player2': 1,
     'Player2Shoot': 25,
-    'Enemy1'   : 1,
+    'Enemy1': 1,
     'Enemy1Shoot': 20,
-    'Enemy2'   : 1,
+    'Enemy2': 1,
     'Enemy2Shoot': 15,
-    'Enemy3'   : 1,
+    'Enemy3': 1,
     'Enemy3Shoot': 12,
 }
 
@@ -61,82 +61,82 @@ ENTITY_HEALTH = {
     'Level2Bg6': 999,
     'Level2Bg7': 999,
     'Level2Bg8': 999,
-    'Player1'  : 300,
+    'Player1': 300,
     'Player1Shoot': 1,
-    'Player2'  : 300,
+    'Player2': 300,
     'Player2Shoot': 1,
-    'Enemy1'   : 100,
+    'Enemy1': 100,
     'Enemy1Shoot': 1,
-    'Enemy2'   : 150,
+    'Enemy2': 150,
     'Enemy2Shoot': 1,
-    'Enemy3'   : 200,
+    'Enemy3': 200,
     'Enemy3Shoot': 1,
 }
 
 ENTITY_SHOOT_DELAY = {
-    'Player1' : 20,
-    'Player2' : 15,
-    'Enemy1'  : 100,
-    'Enemy2'  : 200,
-    'Enemy3'  : 250,
+    'Player1': 20,
+    'Player2': 15,
+    'Enemy1': 100,
+    'Enemy2': 200,
+    'Enemy3': 250,
 }
 
 ENTITY_SPEED = {
-    'Level1Bg0'    : 0,
-    'Level1Bg1'    : 1,
-    'Level1Bg2'    : 2,
-    'Level1Bg3'    : 3,
-    'Level1Bg4'    : 4,
-    'Level1Bg5'    : 5,
-    'Level1Bg6'    : 6,
-    'Level2Bg0'    : 0,
-    'Level2Bg1'    : 1,
-    'Level2Bg2'    : 2,
-    'Level2Bg3'    : 3,
-    'Level2Bg4'    : 4,
-    'Level2Bg5'    : 5,
-    'Level2Bg6'    : 6,
-    'Level2Bg7'    : 7,
-    'Level2Bg8'    : 8,
-    'Player1'      : 3,
-    'Player1Shoot' : 5,
-    'Player2'      : 3,
-    'Player2Shoot' : 3,
-    'Enemy1'       : 1,
-    'Enemy1Shoot'  : 5,
-    'Enemy2'       : 1,
-    'Enemy2Shoot'  : 2,
-    'Enemy3'       : 1,
-    'Enemy3Shoot'  : 2,
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Level1Bg4': 4,
+    'Level1Bg5': 5,
+    'Level1Bg6': 6,
+    'Level2Bg0': 0,
+    'Level2Bg1': 1,
+    'Level2Bg2': 2,
+    'Level2Bg3': 3,
+    'Level2Bg4': 4,
+    'Level2Bg5': 5,
+    'Level2Bg6': 6,
+    'Level2Bg7': 7,
+    'Level2Bg8': 8,
+    'Player1': 3,
+    'Player1Shoot': 5,
+    'Player2': 3,
+    'Player2Shoot': 3,
+    'Enemy1': 1,
+    'Enemy1Shoot': 5,
+    'Enemy2': 1,
+    'Enemy2Shoot': 2,
+    'Enemy3': 1,
+    'Enemy3Shoot': 2,
 }
 
 ENTITY_SCORE = {
-    'Level1Bg0'    : 0,
-    'Level1Bg1'    : 0,
-    'Level1Bg2'    : 0,
-    'Level1Bg3'    : 0,
-    'Level1Bg4'    : 0,
-    'Level1Bg5'    : 0,
-    'Level1Bg6'    : 0,
-    'Level2Bg0'    : 0,
-    'Level2Bg1'    : 0,
-    'Level2Bg2'    : 0,
-    'Level2Bg3'    : 0,
-    'Level2Bg4'    : 0,
-    'Level2Bg5'    : 0,
-    'Level2Bg6'    : 0,
-    'Level2Bg7'    : 0,
-    'Level2Bg8'    : 0,
-    'Player1'      : 0,
-    'Player1Shoot' : 0,
-    'Player2'      : 0,
-    'Player2Shoot' : 0,
-    'Enemy1'       : 100,
-    'Enemy1Shoot'  : 0,
-    'Enemy2'       : 125,
-    'Enemy2Shoot'  : 0,
-    'Enemy3'       : 150,
-    'Enemy3Shoot'  : 0,
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
+    'Level2Bg5': 0,
+    'Level2Bg6': 0,
+    'Level2Bg7': 0,
+    'Level2Bg8': 0,
+    'Player1': 0,
+    'Player1Shoot': 0,
+    'Player2': 0,
+    'Player2Shoot': 0,
+    'Enemy1': 100,
+    'Enemy1Shoot': 0,
+    'Enemy2': 125,
+    'Enemy2Shoot': 0,
+    'Enemy3': 150,
+    'Enemy3Shoot': 0,
 }
 
 # M
@@ -147,26 +147,41 @@ MENU_OPTION = ('NEW GAME',
                'EXIT')
 
 # P
-PLAYER_KEY_UP =    {'Player1': pygame.K_w,
-                   'Player2':pygame.K_UP}
-PLAYER_KEY_DOWN =  {'Player1': pygame.K_s,
-                   'Player2':pygame.K_DOWN}
-PLAYER_KEY_LEFT =  {'Player1': pygame.K_a,
-                   'Player2':pygame.K_LEFT}
+PLAYER_KEY_UP = {'Player1': pygame.K_w,
+                 'Player2': pygame.K_UP}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_s,
+                   'Player2': pygame.K_DOWN}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_a,
+                   'Player2': pygame.K_LEFT}
 PLAYER_KEY_RIGHT = {'Player1': pygame.K_d,
-                   'Player2':pygame.K_RIGHT}
+                    'Player2': pygame.K_RIGHT}
 PLAYER_KEY_SHOOT = {'Player1': pygame.K_LSHIFT,
-                   'Player2':pygame.K_RSHIFT}
+                    'Player2': pygame.K_RSHIFT}
 
 # S
-SPAWN_TIME = 1000
+SPAWN_TIME = 4000
 
 # T
-TIMEOUT_STEP = 100 # 100ms
-TIMEOUT_LEVEL = 20000 #60 segundos
+TIMEOUT_STEP = 100  # 100ms
+TIMEOUT_LEVEL = 5000  # 60 segundos
 
 #  W
 WIN_WIDTH = 1920
 WIN_HEIGHT = 1080
 
-
+# SCORE
+SCORE_POS = {'Title':     (WIN_WIDTH / 2, 50),
+             'EnterName': (WIN_WIDTH / 2, 80),
+             'Label':     (WIN_WIDTH / 2, 90),
+             'Name':      (WIN_WIDTH / 2, 110),
+             0: (WIN_WIDTH / 2, 110),
+             1: (WIN_WIDTH / 2, 130),
+             2: (WIN_WIDTH / 2, 150),
+             3: (WIN_WIDTH / 2, 170),
+             4: (WIN_WIDTH / 2, 190),
+             5: (WIN_WIDTH / 2, 210),
+             6: (WIN_WIDTH / 2, 230),
+             7: (WIN_WIDTH / 2, 250),
+             8: (WIN_WIDTH / 2, 270),
+             9: (WIN_WIDTH / 2, 290),
+             }
